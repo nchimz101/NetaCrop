936b848c2ff64ab1295328e674c74397883ceec6
Nchimunya Munyama <61247975+nchimz101@users.noreply.github.com>
2022-04-03 21:39:23 +0200

    commit

--- PlantApp/urls.py
+++ PlantApp/urls.py
@@ -15,17 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
-from django.views.static import serve
-from django.conf.urls import url
-
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("DiseaseDetect.urls")),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-]
+    path ('',include("DiseaseDetect.urls")),
+    
 ]
